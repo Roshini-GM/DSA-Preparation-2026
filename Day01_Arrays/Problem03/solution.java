93fe3cd186aa8cef2951b320e03f743d1be8c361
Roshini Muniprasad <roshiniprasad17@gmail.com>
2026-09-09 20:57:07 +0530

    solution.java

--- Day01_Arrays/Problem03/solution.java
+++ Day01_Arrays/Problem03/solution.java
@@ -1,5 +1,13 @@
 
-
-public class solution {
-    
+class Solution {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+        for(int x : nums) {
+            if(String.valueOf(x).length() % 2 == 0){
+                ++ans;
+            }
+        }
+        return ans;
+    }
 }
+
