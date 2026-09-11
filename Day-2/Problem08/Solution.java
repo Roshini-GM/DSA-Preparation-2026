37218270a9371b30487beff7063e44f9a125f01e
Roshini Muniprasad <roshiniprasad17@gmail.com>
2026-09-11 09:51:03 +0530

    Update Solution.java

--- Day-2/Problem08/Solution.java
+++ Day-2/Problem08/Solution.java
@@ -1,5 +1,17 @@
-package Problem08;
-
-public class Solution {
-    
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int x : nums) {
+            cnt.merge(x, 1, Integer::sum);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq
+            = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        for (var e : cnt.entrySet()) {
+            pq.offer(e);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.stream().mapToInt(Map.Entry::getKey).toArray();
+    }
 }
