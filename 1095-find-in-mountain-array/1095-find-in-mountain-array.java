0be479be1fc5e016daf76908724ede3b40ca9324
Roshini Muniprasad <roshiniprasad17@gmail.com>
2026-09-18 20:03:59 +0530

    Time: 0 ms (100%), Space: 46.5 MB (60.54%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n = mountainArr.length();
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        int peak = left;
+        int ans = binarySearch(mountainArr, target, 0, peak, true);
+        if (ans != -1) {
+            return ans;
+        }
+        return binarySearch(mountainArr, target, peak + 1, n - 1, false);
+    }
+    private int binarySearch(MountainArray arr, int target,
+                              int left, int right, boolean increasing) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int value = arr.get(mid);
+            if (value == target) {
+                return mid;
+            }
+            if (increasing) {
+                if (value < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (value < target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
